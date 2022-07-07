c1ec9fd9bcc916fba73a9e75797b637617885f6a
starcush <hacker0419@gmail.com>
2022-07-07 22:37:38 +0900

    add BV

--- src/bonus.js
+++ src/bonus.js
@@ -1,40 +1,52 @@
 import { PV_BV_RATIO } from './constants';
 
 export function applyTargetBonus(cy, element) {
+  const curSelDiv = document.querySelector('.cur-sel');
+  const curPvDiv = document.querySelector('.cur-pv');
+  const curPaybackDiv = document.querySelector('.cur-payback');
+  const curReward1stDiv = document.querySelector('.cur-reward-1st');
+  const curReward2ndDiv = document.querySelector('.cur-reward-2nd');
+  const curRewardDiv = document.querySelector('.cur-reward');
+
+  const show2ndReward = () => {
+    curReward2ndDiv.parentElement.classList.remove('hide');
+    curRewardDiv.parentElement.classList.remove('hide');
+  };
+
+  const hide2ndReward = () => {
+    curReward2ndDiv.parentElement.classList.add('hide');
+    curRewardDiv.parentElement.classList.add('hide');
+  };
+
   if (cy == null || element == null) {
-    const curSelDiv = document.querySelector('.cur-sel');
     curSelDiv.innerHTML = '없음';
-    const curPvDiv = document.querySelector('.cur-pv');
     curPvDiv.innerHTML = '0 만';
-    const curPaybackDiv = document.querySelector('.cur-payback');
     curPaybackDiv.innerHTML = '0 %';
-    const curReward1stDiv = document.querySelector('.cur-reward-1st');
     curReward1stDiv.innerHTML = '0 만원';
-    const curReward2ndDiv = document.querySelector('.cur-reward-2nd');
     curReward2ndDiv.innerHTML = '0 만원';
-    const curRewardDiv = document.querySelector('.cur-reward');
     curRewardDiv.innerHTML = '0 만원';
+    hide2ndReward();
     return;
   }
 
-  const curSelDiv = document.querySelector('.cur-sel');
   curSelDiv.innerHTML = element.data('label');
 
   const { totalPv, ratio, reward1st } = calc1stBonus(cy, element);
-  const curPvDiv = document.querySelector('.cur-pv');
   curPvDiv.innerHTML = `${totalPv} 만`;
-  const curPaybackDiv = document.querySelector('.cur-payback');
   curPaybackDiv.innerHTML = `${ratio * 100} %`;
-  const curReward1stDiv = document.querySelector('.cur-reward-1st');
-  curReward1stDiv.innerHTML = `${reward1st.toFixed(1)} 만원`;
+  curReward1stDiv.innerHTML = `${reward1st.toFixed(2)} 만원`;
 
   const reward2nd = calc2ndBonus(cy, element, totalPv, reward1st);
-  const curReward2ndDiv = document.querySelector('.cur-reward-2nd');
-  curReward2ndDiv.innerHTML = `${reward2nd.toFixed(1)} 만원`;
-
-  const curRewardDiv = document.querySelector('.cur-reward');
-  const totalReward = (reward1st + reward2nd) * PV_BV_RATIO;
-  curRewardDiv.innerHTML = `${totalReward.toFixed(2)} 만원`;
+  if (reward2nd) {
+    show2ndReward();
+    curReward2ndDiv.innerHTML = `${reward2nd.toFixed(2)} 만원`;
+    const totalReward = reward1st + reward2nd;
+    curRewardDiv.innerHTML = `${totalReward.toFixed(2)} 만원`;
+  } else {
+    hide2ndReward();
+    curReward2ndDiv.innerHTML = '0 만원';
+    curRewardDiv.innerHTML = '0 만원';
+  }
 }
 
 let tempPv = 0;
@@ -43,7 +55,8 @@ function calc1stBonus(cy, element) {
   calcTargetBonus(cy, element);
   const totalPv = tempPv;
   let ratio = getPercentByPV(totalPv);
-  const reward1st = calcTargetReward(cy, element, ratio * totalPv);
+  const total1Reward = ratio * totalPv * PV_BV_RATIO;
+  const reward1st = calcTargetReward(cy, element, total1Reward);
   return { totalPv, ratio, reward1st };
 }
 
@@ -77,7 +90,8 @@ function calcTargetReward(cy, element, totalReward) {
     calcTargetBonus(cy, child);
     const childPv = tempPv;
     const childRatio = getPercentByPV(childPv);
-    totalReward -= childPv * childRatio;
+    const childTotalBonus = childPv * childRatio * PV_BV_RATIO;
+    totalReward -= childTotalBonus;
   });
   return totalReward;
 }
@@ -105,7 +119,6 @@ function calc2ndBonus(cy, element, totalPv, reward1st) {
   children.forEach(function (child) {
     tempPv = parseInt(child.data('pv'));
     calcTargetBonus(cy, child);
-    console.log(tempPv);
     if (tempPv < 60) bFail = true;
   });
   if (bFail) return 0;
